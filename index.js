af607bb9cc628fa82e26ffe93710c7865321c594
ֆαɾѵҽsɦ Ꮆαυɾ <90101116+sarveshgaur@users.noreply.github.com>
2022-06-19 00:03:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function validate {
+  var  username=document.getElementById('username').value;
+  var  password=document.getElementById('password').value;
+  if ( username =="admin" && password == "user"){
+alert("login successful");
+return false;
+  }
+  else{
+      alert("user not found");
+  }
+}
