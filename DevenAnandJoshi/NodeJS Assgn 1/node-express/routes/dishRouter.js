033d00592de2e51f2310101bfe466d4bb746a6c8
DevenJoshi <jdevena30@gmail.com>
2022-07-11 23:27:35 +0530

    edit

--- DevenAnandJoshi/NodeJS Assgn 1/node-express/routes/dishRouter.js
+++ DevenAnandJoshi/NodeJS Assgn 1/node-express/routes/dishRouter.js
@@ -5,7 +5,7 @@ const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
-dishRouter.route('/:leaderId')
+dishRouter.route('/:dishId')
 .all((req,res,next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
